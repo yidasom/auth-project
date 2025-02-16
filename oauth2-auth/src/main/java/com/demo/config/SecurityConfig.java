dc6d58c14cb7fef21cfb110c1a1ccccfddeea36e
Yi da-som <somlh1212@gmail.com>
2025-02-16 18:11:58 +0900

    oauth2 client 설정 코밋

---
+++ oauth2-auth/src/main/java/com/demo/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * com.demo.config
+ *
+ * @author : idasom
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/auth/oauth/*").permitAll()
+                .anyRequest().authenticated()
+        ).oauth2Login(oauth2 -> oauth2
+                .defaultSuccessUrl("/home")
+        );
+        return http.build();
+    }
+}
